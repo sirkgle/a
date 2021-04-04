541a6d35ca11c08725888ce0fd5c0ff234f8f271
sirkgle <79742439+sirkgle@users.noreply.github.com>
2021-04-04 12:59:20 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,3 @@
+var header = document.querySelector('header'),ul = document.querySelector('ul');
+ul.addEventListener('mouseover',function(){header.style.height='600px';});
+ul.addEventListener('mouseout',function(){header.style.height='50px';});
