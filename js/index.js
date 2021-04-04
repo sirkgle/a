6186f1ad056831dad2330ceb9fc86da261a3ba68
sirkgle <79742439+sirkgle@users.noreply.github.com>
2021-04-04 14:17:04 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,3 @@
 var header = document.querySelector('header'),ul = document.querySelector('ul');
-ul.addEventListener('mouseover',function(){header.style.height='600px';});
+ul.addEventListener('mouseover',function(){header.style.height='625px';});
 ul.addEventListener('mouseout',function(){header.style.height='50px';});
